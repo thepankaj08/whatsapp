dd6b9dc1ae76493a3cedce74ab67ee4591ea7a56
thepankaj08 <82504685+thepankaj08@users.noreply.github.com>
2021-06-06 13:06:40 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,8 @@
+function submit() {
+			var inp
+			inp = document.getElementById('number').value
+			//document.getElementById('result').innerHTML = `wa.me/91${inp}`;
+			window.open(`https://wa.me/91${inp}`);
+
+
+		}
